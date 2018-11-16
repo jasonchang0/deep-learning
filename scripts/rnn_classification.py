9fcf6cb961cf530ff7eaa9575196c9ee6ddc0f03
Jason Chang <jasonchang@Jasons-MacBook-Pro-2.local>
2018-11-16 01:46:54 -0800

    apply rnn to pandas dataframes

--- scripts/rnn_classification.py
+++ scripts/rnn_classification.py
@@ -15,15 +15,16 @@ df = pd.read_csv(filepath_or_buffer=filename, compression='gzip')
 
 df = utils.shuffle(df, random_state=None, n_samples=None)
 
+hm_epochs = 10
 n_classes = 2
 batch_size = 128
 
-chunk_size = 502
-n_chunks = 1
+chunk_size = 251
+n_chunks = 2
 rnn_size = 512
 
 x = tf.placeholder('float', [None, n_chunks, chunk_size])
-y = tf.placeholder('float')
+y = tf.placeholder('int32', [batch_size])
 
 X = df.drop('label', axis=1, inplace=False)
 X = X.astype(float)
@@ -42,10 +43,12 @@ Y = df[['label']].astype(int)
 # Y = np.array(Y).astype(int)
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)
+y_train = y_train.astype(int)
+y_test = y_test.astype(int)
 
-lb = LabelBinarizer()
-y_train = lb.fit_transform(y_train).tolist()
-y_test = lb.fit_transform(y_test).tolist()
+# lb = LabelBinarizer()
+# y_train = lb.fit_transform(y_train).tolist()
+# y_test = lb.fit_transform(y_test).tolist()
 
 
 def recurrent_neural_network(x):
@@ -59,7 +62,7 @@ def recurrent_neural_network(x):
     lstm_cell = rnn_cell.BasicLSTMCell(rnn_size)
     outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
 
-    output = tf.matmul(outputs[-1], layer['weights']) + layer['biases']
+    output = tf.add(tf.matmul(outputs[-1], layer['weights']), layer['biases'])
 
     return output
 
@@ -85,10 +88,9 @@ def train_neural_network(x):
     # print(prediction.get_shape())
     # print(y.get_shape())
 
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+    cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-    hm_epochs = 10
     with tf.Session() as sess:
         # OLD:
         # sess.run(tf.initialize_all_variables())
@@ -99,7 +101,8 @@ def train_neural_network(x):
             epoch_loss = 0
             for _ in range(int(len(y_train) / batch_size)):
                 epoch_x, epoch_y = next_batch(x_train, y_train, _, batch_size)
-                epoch_x = epoch_x.reshape(batch_size, 1, chunk_size)
+                epoch_x = epoch_x.reshape(batch_size, n_chunks, chunk_size)
+                epoch_y = epoch_y.reshape(batch_size, )
 
                 _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
@@ -109,11 +112,12 @@ def train_neural_network(x):
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
 
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x: x_test.reshape(-1, 1, chunk_size),
-                                          y: y_test}))
+        print('Accuracy:', accuracy.eval({x: np.array(x_test).reshape(-1, n_chunks, chunk_size),
+                                          y: np.array(y_test).reshape(batch_size, )}))
 
 
 train_neural_network(x)
+
 
 
 
